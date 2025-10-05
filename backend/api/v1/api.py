3b5aafb18ae9c8fa95fa37c586941e488a778f85
CraftApp Automation <no-reply@craftapp.com>
2025-10-05 07:18:38 +0000

    Initial master branch

--- backend/api/v1/api.py
+++
@@ -1,7 +0,0 @@
-# backend/api/v1/api.py
-from fastapi import APIRouter
-from api.v1.endpoints import assets
-
-router = APIRouter()
-
-router.include_router(assets.router, prefix="/assets", tags=["assets"])
